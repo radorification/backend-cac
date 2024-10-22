6d22bf4c5f9a5165ce880958a6a19ffa191695aa
radorification <kumarankur131@gmail.com>
2024-10-22 17:09:53 +0530

    multer config done!!

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@ dotenv.config({
     path: "./env"
 });
 import connectDB from "./db/index.js";
+import express from "express";
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("<h3>Connection established!</h3>");
+})
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is running on port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error", err);
+    
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,43 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+    })
+
+
+
+
+    const uploadOnCloudinary = async (localFilePath) => {
+        try {
+            if(!localFilePath)  return null;
+            // storing file on Cloudinary
+            const response = await cloudinary.uploader.upload(
+                localFilePath, {
+                    resource_type: "auto"
+                }
+            )
+            // when file is successfully uploaded on cloudinary
+            console.log("File successfully uploaded on cloudinary", response.url);
+            return response;
+        } catch (error) {
+            fs.unlinkSync(localFilePath) //removes the locally stored temporary file as upload failed!
+            return null;
+        }
+    }
+
+
+    // const uploadResult = await cloudinary.uploader
+    //    .upload(
+    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+    //            public_id: 'shoes',
+    //        }
+    //    )
+    //    .catch((error) => {
+    //        console.log(error);
+    //    });
+    
+    // console.log(uploadResult);
